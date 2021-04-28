eb241f989e6d5ca3a16f4181161a1f2a79c1ac4e
santt3131 <santt31@gmail.com>
2021-04-28 18:27:43 +0200

    clean app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,18 +10,18 @@ import { ActivitiesAdminListComponent } from './activities/components/activities
 import { ActivityAdminDetailComponent } from './activities/components/activity-admin-detail/activity-admin-detail.component';
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent },//
+    { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'activity-list', component: ActivityListComponent },
     { path: 'activity-list/:id', component: ActivityListComponent },
     { path: 'activity-list-user/favorites', component: ActivityListComponent },
-    { path: 'profile-language/:uid', component: ProfileLanguageComponent },//
-    { path: 'profile-language', component: ProfileLanguageComponent },//
-    { path: 'profile-education/:uid', component: ProfileEducationComponent },//
-    { path: 'profile-education', component: ProfileEducationComponent },//
+    { path: 'profile-language/:uid', component: ProfileLanguageComponent },
+    { path: 'profile-language', component: ProfileLanguageComponent },
+    { path: 'profile-education/:uid', component: ProfileEducationComponent },
+    { path: 'profile-education', component: ProfileEducationComponent },
     { path: 'activities-admin-list', component: ActivitiesAdminListComponent },
-    { path: 'activity-admin-detail/:id', component: ActivityAdminDetailComponent },//
+    { path: 'activity-admin-detail/:id', component: ActivityAdminDetailComponent },
     { path: 'activity-admin-detail', component: ActivityAdminDetailComponent },
     // esta última sirve para ofrecer una ruta que se abra en el raiz de la app
     { path: '', redirectTo: '/activity-list', pathMatch: 'full' }
